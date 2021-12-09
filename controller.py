073fb8e19014a49fbcd0cfe6b59ae0055b25baea
weburken <weburken@iastate.edu>
2021-12-08 21:22:43 -0600

    Fixed controller

--- controller.py
+++ controller.py
@@ -1,28 +1,60 @@
 import multiprocessing
+import subprocess
 import time
 import os
 
 # Your foo function
 def foo(n):
-    for i in range(10000 * n):
-        print( "Tick")
-        time.sleep(1)
+    #os.system("roslaunch final_project main.launch")
+    child = subprocess.Popen(["roslaunch","final_project","main.launch"])
+    try:
+        child.wait(100)
+    except:
+        child.kill()
 
 if __name__ == '__main__':
+    #os.system("roslaunch final_project main.launch")
+
+
     # Start foo as a process
-
-
-
-    Rossy = os.system("roslaunch final_project main.launch")
-    time.sleep(10)
-    Rossy.terminate()
-    Rossy.join()
-
-    print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nDONE\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
     x = 10
     y = 10
 
     #TEST: Below is testing material
+    roslaunch = subprocess.Popen(["roslaunch","final_project","main.launch"])
+    try:
+        roslaunch.wait(100)
+    except:
+        roslaunch.kill()
+
+
+
+    time.sleep(5)
+    Robot1 = subprocess.Popen(["rosrun", "final_project","robot1.py"])
+
+    time.sleep(5)
+    Robot1.kill()
+    #roslaunch.kill()
+
+
+
+    """
+    p = multiprocessing.Process(target=foo, name="Foo", args=(10,))
+    p.start()
+    print("Thats a win")
+    time.sleep(10)
+    p.join(20)
+
+    # If thread is active
+    if p.is_alive():
+        print("foo is running... let's kill it...")
+        # Terminate foo
+        #os.system("kill 3097")
+        p.terminate()
+        p.join()
+
+
+    
     p = multiprocessing.Process(target=foo, name="Foo", args=(10,))
     p.start()
 
@@ -36,21 +68,7 @@ if __name__ == '__main__':
         p.terminate()
         p.join()
         #finList.append(20)
-
-
-    p = multiprocessing.Process(target=foo, name="Foo", args=(10,))
-    p.start()
-
-    p.join(20)
-
-    # If thread is active
-    if p.is_alive():
-        print("foo is running... let's kill it...")
-
-        # Terminate foo
-        p.terminate()
-        p.join()
-        #finList.append(20)
+    """
 #TEST: Above is testing material
 
 
